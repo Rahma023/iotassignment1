a7dab51276097527efb310d3f446f58e3f24b1df
student <rahmafaizyusuf@gmail.com>
2021-04-24 07:14:50 +0300

    first commit

---
+++ IOT ASSIGNMENT/iot_assignment_1/Green.ino
@@ -0,0 +1,9 @@
+void GREEN(){
+  digitalWrite(Green,HIGH);
+  delay(1000);
+  digitalWrite(Green,LOW);
+  delay(1000);
+
+  
+  
+  }
---
+++ IOT ASSIGNMENT/iot_assignment_1/Red.ino
@@ -0,0 +1,8 @@
+void RED(){
+  digitalWrite(Red,HIGH);
+  delay(1000);
+  digitalWrite(Red,LOW);
+  delay(1000);
+
+  
+  }
